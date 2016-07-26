009b77d2842c3b20ee848487c0d81161cc01f780
Malaka <Malaka@DESKTOP-5HIQH93>
2016-07-26 12:01:04 +0530

    add MainWindow and customer - add and edit frames.

---
+++ src/view/customers/FrmAddCustomerController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view.customers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Malaka
+ */
+public class FrmAddCustomerController implements Initializable {
+
+    @FXML
+    private Button btnSave;
+    @FXML
+    private Button btnClose;
+    @FXML
+    private TextField txtCusID;
+    @FXML
+    private TextField txtCusFName;
+    @FXML
+    private TextField txtCusLName;
+    @FXML
+    private TextField txtCusTelephone;
+    @FXML
+    private TextArea txtCusAddress;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void btnSave_onAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void btnClose_onClose(ActionEvent event) {
+    }
+    
+}
